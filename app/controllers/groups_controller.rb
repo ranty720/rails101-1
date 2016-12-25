b238dee06faa4a38768b639fa005de0c10f3ce2e
ranty <ranty720@gmail.com>
2016-12-25 21:40:34 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,6 +12,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
@@ -53,7 +54,7 @@ class GroupsController < ApplicationController
     end
   end
 
-    
+
 
   def group_params
     params.require(:group).permit(:title, :description)
